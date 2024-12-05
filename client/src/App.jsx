b1ab9ae981fa6b04ae4cfbc3f2f95e326de68f1a
yh97777 <yh97777@naver.com>
2024-12-05 14:19:43 +0900

    메인헤더

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Tap from "./Components/Tap/Tap";
 import "./App.css";
+import Main from "./Components/Main/Main";
 
 function App() {
   return (
     <>
+      <Main />
       <Tap />
     </>
   );
---
+++ client/src/Components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import MainHeader from "./MainHeader/MainHeader";
+import MainBody from "./MainBody/MainBody";
+
+function Main() {
+  return (
+    <>
+      <MainHeader />
+      <MainBody />
+    </>
+  );
+}
+
+export default Main;
