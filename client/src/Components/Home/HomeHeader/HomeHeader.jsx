0a767a84053e0f24bb5805873819f4896d401295
yh97777 <yh97777@naver.com>
2024-12-10 13:11:21 +0900

    홈화면 퍼블리싱

--- client/src/Components/Home/HomeHeader/HomeHeader.jsx
+++ client/src/Components/Home/HomeHeader/HomeHeader.jsx
@@ -3,16 +3,18 @@ import StarBook from "../../../assets/img/Main/mainimg.svg";
 
 function HomeHeader() {
   return (
-    <section className={styles.mainHeaderBox}>
+    <div className={styles.mainHeaderBox}>
       <div>
         <img className={styles.mainImg} src={StarBook} alt="메인이미지" />
       </div>
-      <div className={styles.mainHeaderText}>
-        독립서점에서 찾은
-        <br />특<span className={styles.mainStarColor}>별</span>한 책, 특<span className={styles.mainStarColor}>별</span>한 순간
+      <div className={styles.header_text_warp}>
+        <div className={styles.mainHeaderText}>
+          대구 독립서점에서 찾은
+          <br />특<span className={styles.mainStarColor}>별</span>한 책, 특<span className={styles.mainStarColor}>별</span>한 순간
+        </div>
+        <div className={styles.mainHeaderSubText}>QR 코드를 스캔하여 쿠폰을 받으세요.</div>
       </div>
-      <div className={styles.mainHeaderSubText}>QR 코드를 스캔하여 선물을 받으세요.</div>
-    </section>
+    </div>
   );
 }
 
