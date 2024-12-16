7ce83920faf67bf0ea68f3d4ec2e5904c5c62cc5
ghks6455 <ghks6455@gmail.com>
2024-12-16 14:13:30 +0900

    비트 설정파일 수정

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        format: "es",
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash][extname]",
+      },
+    },
+  },
+  // 새로 고침 하면 인덱스로 보낸다는데 잘 모르곘음
+  server: {
+    historyApiFallback: true,
+  },
 });
