872d8528dce5c58fcbe03c4f899ff2a74502c1ce
ghks6455 <ghks6455@gmail.com>
2024-12-16 11:42:47 +0900

    배포 작업중

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,30 +6,30 @@ import "./index.css";
 
 // 컴포넌트
 // 스플레시
-import Splash from "./Components/Splash/Splash";
+import Splash from "./Components/Splash/Splash.jsx";
 // 레이아웃 컴포넌트 (푸터 출력을 여기서 설정)
-import StarBook from "./components/StarBook/Starbook";
+import StarBook from "./components/StarBook/Starbook.jsx";
 // 메인
 import Home from "./Components/Home/Home";
 // 지도
-import Location from "./Components/Location/Location";
+import Location from "./Components/Location/Location.jsx";
 //QR
-import Qr from "./Components/Qr/Qr";
+import Qr from "./Components/Qr/Qr.jsx";
 // 스탬프
-import Stamp from "./Components/Stamp/Stamp";
+import Stamp from "./Components/Stamp/Stamp.jsx";
 // 마이페이지, 회원정보, 쿠폰
-import Account from "./Components/Account/Account";
-import Account_detail from "./Components/Account_detail/Account_detail";
-import Coupon from "./Components/Coupon/Coupon";
+import Account from "./Components/Account/Account.jsx";
+import Account_detail from "./Components/Account_detail/Account_detail.jsx";
+import Coupon from "./Components/Coupon/Coupon.jsx";
 // 로그인과 회원가입
-import Login from "./Components/Login/Login";
-import SignUp from "./Components/SignUp/SignUp";
+import Login from "./Components/Login/Login.jsx";
+import SignUp from "./Components/SignUp/SignUp.jsx";
 // 북마크 피드, 게시글 작성, 댓글 작성
-import Bookmark from "./Components/Bookmark/Bookmark";
-import Write from "./Components/Write/Write";
-import Comment from "./Components/Comment/Comment";
+import Bookmark from "./Components/Bookmark/Bookmark.jsx";
+import Write from "./Components/Write/Write.jsx";
+import Comment from "./Components/Comment/Comment.jsx";
 // 카카오 로그인 리다이렉트 URL
-import Auth from "./Components/Auth/Auth";
+import Auth from "./Components/Auth/Auth.jsx";
 
 function App() {
   return (
