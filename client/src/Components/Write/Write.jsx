629112bc9b1ff23edda1ede0b4c14f72e47b2a7f
yh97777 <yh97777@naver.com>
2024-12-17 17:31:59 +0900

    전반적인 css조정

--- client/src/Components/Write/Write.jsx
+++ client/src/Components/Write/Write.jsx
@@ -1,17 +1,12 @@
 import Header from "../Header/Header";
 import styles from "./Write.module.css";
 import bookmark from "../../assets/img/Bookmark/thumnail.png";
+import pirture from "../../assets/img/Bookmark/picture.png";
 // className={styles.}
 export default function Write() {
   return (
     <>
-      <Header
-        showBackButton={true}
-        backButtonColor="#FFFFFF"
-        showSubmitButton={true}
-        buttonText={"등록"}
-        color="#FFFFFF"
-      />
+      <Header showBackButton={true} backButtonColor="#FFFFFF" showSubmitButton={true} buttonText={"등록"} color="#FFFFFF" />
       <section className={styles.Write}>
         <form className={styles.Write_form} action="">
           <div className={styles.tag}>
@@ -23,8 +18,10 @@ export default function Write() {
           </div>
           {/* 사진 등록 할 위치  */}
           <div className={styles.photo}>
-            <div className={styles.photo_upload}></div>
-            <img src={bookmark} className={styles.photo_uploaded} />
+            <div className={styles.photo_upload}>
+              <img src={pirture} />
+            </div>
+            <div className={styles.photo_uploaded}></div>
           </div>
         </form>
       </section>
