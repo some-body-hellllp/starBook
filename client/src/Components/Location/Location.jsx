595b9ca5efffc1b9c995ef60e0359489b125a04a
yh97777 <yh97777@naver.com>
2024-12-17 13:40:10 +0900

    지도완성 및 qr확인

--- client/src/Components/Location/Location.jsx
+++ client/src/Components/Location/Location.jsx
@@ -1,33 +1,35 @@
-
 import { useEffect } from "react";
 import ReactDOMServer from "react-dom/server"; // React 컴포넌트를 문자열로 변환하기 위한 라이브러리
 import Header from "../Header/Header";
 import locationImg from "../../assets/img/Location/locationImg.svg";
 import "./Location.css";
 import LocationModal from "./LocationModal/LocationModal";
+import areteinliterature from "../../assets/img/Location/areteinliterature.jpg";
+import chabangbookstore from "../../assets/img/Location/chabangbookstore.jpg";
+import chaegbangdadog from "../../assets/img/Location/chaegbangdadog.jpg";
+import chaegbangsilgyeog from "../../assets/img/Location/chaegbangsilgyeog.jpg";
+import cornerbooks from "../../assets/img/Location/cornerbooks.jpg";
+import daebongwalk from "../../assets/img/Location/daebongwalk.jpg";
+import damdambookstore from "../../assets/img/Location/damdambookstore.jpg";
+import ghostbooks from "../../assets/img/Location/ghostbooks.jpg";
+import ilgeulchaek from "../../assets/img/Location/ilgeulchaek.jpg";
+import pollock from "../../assets/img/Location/pollock.jpg";
+import publicbookstore from "../../assets/img/Location/publicbookstore.jpg";
+import simplebookstore from "../../assets/img/Location/simplebookstore.jpg";
+import surchag from "../../assets/img/Location/surchag.jpg";
+import travelerbook from "../../assets/img/Location/travelerbook.jpg";
+import sasohanchaegbang from "../../assets/img/Location/sasohanchaegbang.jpg";
 
 export default function Location() {
-  // 위도, 경도 가져온 후 카카오맵 함수로 넘김
+  // 위치기반을 끄려면 이 부분을 수정
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          // 카카오맵 그리기
-          printKakaomap(latitude, longitude);
-        },
-        (error) => {
-          console.log(error);
-        },
-        {
-          enableHighAccuracy: true, // 정확한 위치 정보를 요청
-          timeout: 10000, // 위치 정보 요청 시간 제한 (10초)
-          maximumAge: 0, // 캐시된 위치 정보를 사용하지 않음
-        }
-      );
-    } else {
-      console.error("브라우저가 Geolocation API를 지원하지 않습니다. ");
-    }
+    // 위치 기반을 사용하지 않도록 코드 제거
+    // 기존에는 위치를 가져왔지만, 이제는 그냥 고정된 좌표로 맵을 그리게 변경
+    const defaultLatitude = 35.87; // 예시로 대구의 중심 위치
+    const defaultLongitude = 128.6;
+
+    // 카카오맵 그리기
+    printKakaomap(defaultLatitude, defaultLongitude);
   }, []);
 
   // 카카오맵 그리기
@@ -45,90 +47,105 @@ export default function Location() {
         adress: "대구광역시 서구 달서로 81-2 3층",
         number: "053-565-3469",
         latlng: new kakao.maps.LatLng(35.86967, 128.5708),
+        img: damdambookstore,
       },
       {
         title: "더폴락",
         adress: "대구광역시 중구 성내2동 경상감영1길 62-5",
         number: "010-2977-6533",
         latlng: new kakao.maps.LatLng(35.87376, 128.5932),
+        img: pollock,
       },
       {
         title: "고스트북스",
         adress: "대구광역시 중구 동문동 14-1",
         number: "053-256-2123",
         latlng: new kakao.maps.LatLng(35.87173, 128.5989),
+        img: ghostbooks,
       },
       {
         title: "차방책방",
         adress: "대구광역시 중구 경상감영길 60",
         number: "053-353-4878",
         latlng: new kakao.maps.LatLng(35.87153, 128.5907),
+        img: chabangbookstore,
       },
       {
         title: "수르채그",
         adress: "대구광역시 서구 통학로21길 22 1층",
         number: "010-4566-1710",
         latlng: new kakao.maps.LatLng(35.86942, 128.5609),
+        img: surchag,
       },
       {
         title: "일글책",
         adress: "대구광역시 남구 계명중앙1길 42 1층",
         number: "010-1111-1111",
         latlng: new kakao.maps.LatLng(35.856, 128.5821),
+        img: ilgeulchaek,
       },
       {
         title: "심플책방",
         adress: "대구광역시 동구 신천4동 동부로34길 4",
         number: "010-2222-2222",
         latlng: new kakao.maps.LatLng(35.87719, 128.6306),
+        img: simplebookstore,
       },
       {
         title: "여행자의 책",
         adress: "대구광역시 동구 불로동 1000-51",
         number: "053-219-8080",
         latlng: new kakao.maps.LatLng(35.89919, 128.633),
+        img: travelerbook,
       },
       {
         title: "사소한 책방",
         adress: "대구광역시 서구 당산로 210-43",
         number: "010-8884-3009",
         latlng: new kakao.maps.LatLng(35.85592, 128.5491),
+        img: sasohanchaegbang,
       },
       {
         title: "대봉산책",
         adress: "대구광역시 중구 대봉2동 명덕로 249",
         number: "010-3333-3333",
         latlng: new kakao.maps.LatLng(35.8565, 128.598),
+        img: daebongwalk,
       },
       {
         title: "책방공공",
         adress: "대구광역시 중구 봉산동 223-6",
         number: "010-4444-4444",
         latlng: new kakao.maps.LatLng(35.86213, 128.5976),
+        img: publicbookstore,
       },
       {
         title: "책방실격",
         adress: "대구광역시 남구 중앙대로47길 24",
         number: "010-6552-2925",
         latlng: new kakao.maps.LatLng(35.85438, 128.589),
+        img: chaegbangsilgyeog,
       },
       {
         title: "더코너북스",
         adress: "대구광역시 남구 대명동 566 15번지",
         number: "0507-1442-4939",
         latlng: new kakao.maps.LatLng(35.85438, 128.5704),
+        img: cornerbooks,
       },
       {
         title: "아레테인문학",
         adress: "대구광역시 수성구 범어동 2206번지 3층",
         number: "010-5555-5555",
         latlng: new kakao.maps.LatLng(35.86231, 128.6227),
+        img: areteinliterature,
       },
       {
         title: "책방다독",
         adress: "대구광역시 남구 대명동 번지 1층 1917-13 101호",
         number: "010-6666-6666",
         latlng: new kakao.maps.LatLng(35.85124, 128.5832),
+        img: chaegbangdadog,
       },
     ];
 
@@ -152,7 +169,7 @@ export default function Location() {
       });
 
       // React 컴포넌트를 문자열로 변환하여 인포윈도우의 content로 설정
-      var infowindowContent = ReactDOMServer.renderToString(<LocationModal title={position.title} adress={position.adress} number={position.number} />);
+      var infowindowContent = ReactDOMServer.renderToString(<LocationModal img={position.img} title={position.title} adress={position.adress} number={position.number} />);
 
       var customOverlay = new kakao.maps.CustomOverlay({
         content: infowindowContent,
--- client/src/Components/Location/LocationModal/LocationModal.jsx
+++ client/src/Components/Location/LocationModal/LocationModal.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./LocationModal.module.css"; // CSS 모듈 import
 
-export default function LocationModal({ title, adress, number }) {
+export default function LocationModal({ title, adress, number, img }) {
   return (
     <div className={styles.LocationModalBox}>
-      <div className={styles.LocationModalImg}>이미지</div>
+      <div className={styles.LocationModalImg}>
+        <img src={img} alt="이미지" />
+      </div>
       <div className={styles.LocationModalText}>
         <div className={styles.LocationModalTitle}>{title}</div>
         <div className={styles.LocationModalAdress}>{adress}</div>
