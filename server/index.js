f294871ab9498bc1ad7434ad77224dcb100e1839
ghks6455 <ghks6455@gmail.com>
2024-12-18 19:24:32 +0900

    포트수정

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.get("/", (req, res) => res.send("linked!"));
 app.use("/", routes);
 
 // 서버 배포시 아래에 있는거 사용
-const port = 3368; // 지금 하루에 한번씩 바꾸는중..이유는모름..
-// const port = process.env.DB_PORT || 3307;
+// const port = 3368; // 지금 하루에 한번씩 바꾸는중..이유는모름..
+const port = process.env.DB_PORT || 3368;
 
 // MySQL 데이터베이스 연결 후 서버 시작
 app.listen(port, () => console.log(`서버가 ${port}번 포트에서 실행 중입니다.`));
