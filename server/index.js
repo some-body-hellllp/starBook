5bf1abdb1be53a3202993a6f3fd410144d207d47
ghks6455 <ghks6455@gmail.com>
2024-12-22 02:15:56 +0900

    서버 수정'

--- server/index.js
+++ server/index.js
@@ -6,26 +6,28 @@ const routes = require("./routes/routes");
 
 const app = express();
 app.use(express.json());
-const allowedOrigins = ["https://star-books.netlify.app"];
 
+// CORS 설정
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "https://star-books.netlify.app", // 배포한 클라이언트 URL
+      "http://localhost:5173", // 로컬 개발 환경 URL (Vite 개발 서버)
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"], // 허용할 HTTP 메서드
+    credentials: true, // 쿠키 전송 허용
   })
 );
 
+// OPTIONS 요청 허용 (Preflight 요청 처리)
+app.options("*", cors());
+
+// 라우팅 설정
 app.get("/", (req, res) => res.send("linked!"));
 app.use("/", routes);
 
-// 서버 배포시 아래에 있는거 사용
-// const port = 3368; // 지금 하루에 한번씩 바꾸는중..이유는모름..
+// 서버 배포시 아래 설정 사용
 const port = process.env.DB_PORT || 3368;
 
-// MySQL 데이터베이스 연결 후 서버 시작
+// 서버 시작
 app.listen(port, () => console.log(`서버가 ${port}번 포트에서 실행 중입니다.`));
