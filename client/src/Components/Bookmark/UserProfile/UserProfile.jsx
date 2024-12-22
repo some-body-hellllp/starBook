c09dee686b2337ee9f614cef8832a4bea75f308c
ghks6455 <ghks6455@gmail.com>
2024-12-22 19:43:43 +0900

    게시글 프로필 적용

--- client/src/Components/Bookmark/UserProfile/UserProfile.jsx
+++ client/src/Components/Bookmark/UserProfile/UserProfile.jsx
@@ -2,10 +2,11 @@ import profile from "../../../assets/img/Bookmark/profile.png";
 // 스타일
 import styles from "./UserProfile.module.css";
 export default function UserProfile({ post }) {
+  const img = post || profile;
   return (
     <>
       <aside className={styles.profile}>
-        <img src={post} alt="profile" />
+        <img src={img} alt="profile" />
       </aside>
     </>
   );
