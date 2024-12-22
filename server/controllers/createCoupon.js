064fb035b75aa9faa7d7db341939c1787b1fdadf
ghks6455 <ghks6455@gmail.com>
2024-12-22 22:42:38 +0900

    쿠폰 발급 수정

--- server/controllers/createCoupon.js
+++ server/controllers/createCoupon.js
@@ -1,5 +1,6 @@
 const db = require("../config/db");
 const { CurrentTime } = require("../config/date");
+
 const createCoupon = async (req, res) => {
   const { userId, stampCard } = req.body;
   const time = CurrentTime();
@@ -21,43 +22,23 @@ const createCoupon = async (req, res) => {
       data: null,
     });
   };
+
   const discount = getCouponRate(stampCard);
   console.log("할인률 :", discount);
-  const QUERY1 = `
-    SELECT coupon_discount_rate
-    FROM COUPONS
-    WHERE user_id = ? AND coupon_discount_rate = ?
-    ORDER BY create_at DESC
-    LIMIT 1;  
-`;
 
-  try {
-    // 중복되는 쿠폰을 확인하는 쿼리 실행
-    const [result] = await db.execute(QUERY1, [userId, discount]);
-
-    // 결과가 있다면 중복된 쿠폰이 존재하는 것
-    if (result.length > 0) {
-      console.log("쿠폰 중복");
-      return res.status(409).json({ status: "error", message: "이미 같은 쿠폰을 받은 적이 있습니다." });
-    }
-
-    const QUERY2 = `
+  const QUERY2 = `
     INSERT INTO COUPONS (user_id, coupon_discount_rate, create_at)
     VALUES (?, ?, ?);
   `;
 
-    try {
-      const result = await db.execute(QUERY2, [userId, discount, time]);
-      console.log(`사용자 에게 ${discount}% 할인 쿠폰이 지급되었습니다.`);
-      console.log(result);
-      return res.status(200).json({ status: "success", message: `${discount}% 할인 쿠폰이 지급되었습니다.` });
-    } catch (error) {
-      console.error("쿠폰 지급 중 오류 발생:", error);
-    }
+  try {
+    const result = await db.execute(QUERY2, [userId, discount, time]);
+    console.log(`사용자 에게 ${discount}% 할인 쿠폰이 지급되었습니다.`);
+    console.log(result);
+    return res.status(200).json({ status: "success", message: `${discount}% 할인 쿠폰이 지급되었습니다.` });
   } catch (error) {
-    console.error("쿠폰 중복 확인 중 오류 발생:", error);
-    console.log("쿠폰 발급 실패");
-    return res.status(409).json({ status: "error", message: "쿠폰 발급 실패" });
+    console.error("쿠폰 지급 중 오류 발생:", error);
+    return res.status(500).json({ status: "error", message: "쿠폰 발급 실패" });
   }
 };
 
