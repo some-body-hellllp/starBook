c07e29cfbb5e33573604b688cded97af0051d008
ghks6455 <ghks6455@gmail.com>
2024-12-22 21:14:08 +0900

    dist 수정

--- client/src/Components/Comment/Comment.jsx
+++ client/src/Components/Comment/Comment.jsx
@@ -37,8 +37,6 @@ export default function Comment() {
     <>
       <Header showCancleButton={true} backgroundColor="#ffffff" />
       <section className={styles.comment}>
-        <CommentItem />
-        <CommentItem />
         {comments.map((comment) => (
           <CommentItem key={comment.comment_id} comment={comment} />
         ))}
